82106c68b6133d49f87ecade1526b36d96c7f535
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2024-06-02 22:45:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct list using given q XOR queries/construct-list-using-given-q-xor-queries.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> constructList(int q, vector<vector<int>> &queries) {
+    vector<int> ans;
+    ans.push_back(0);
+    
+    int currentXor = 0; // To keep track of cumulative XOR value
+    
+    for(int i = 0; i < q; i++){
+        if(queries[i][0] == 0){
+            // Apply the currentXor to the new element before inserting
+            ans.push_back(queries[i][1] ^ currentXor);
+        } else {
+            // Update the cumulative XOR value
+            currentXor ^= queries[i][1];
+        }
+    }
+    
+    // Apply the cumulative XOR to all elements in ans
+    for(int& val : ans) {
+        val ^= currentXor;
+    }
+    
+    // Sort the final list
+    sort(ans.begin(), ans.end());
+    
+    return ans;
+}
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int q;
+        scanf("%d", &q);
+
+        vector<vector<int>> queries(q, vector<int>(2));
+        Matrix::input(queries, q, 2);
+
+        Solution obj;
+        vector<int> res = obj.constructList(q, queries);
+
+        Array::print(res);
+    }
+}
+
+// } Driver Code Ends
