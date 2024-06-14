e9031d71c7e6065e6d7c98e74c5806fad58896ba
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2024-06-14 23:29:50 +0530

    Updated Solution - GfG to GitHub

--- Easy/Armstrong Numbers/armstrong-numbers.cpp
+++ Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -3,37 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
   public:
-    string armstrongNumber(int n){
+    string armstrongNumber(int n) {
         // code here
-        int sum = 0;
-		int temp = n;
-		int c = 0;
-
-
-		//find number of digits i.e. "k"
-		while(temp>0)
-		{
-			c++;
-			temp/=10;
-		}
-
-		//change temp to original number n again to run the loop
-		temp = n;
-
-		//loop to find sum according to given condition
-		while(temp>0)
-		{
-			int ld = temp%10;
-			sum+=pow(ld,c);
-			temp/=10;
-		}
-
-		return sum==n?"Yes":"No";
-
+        
+        int x=  n%10;
+      int y= (n/10)%10;
+      int z = n/100;
+       if(x*x*x + y*y*y +z*z*z == n)
+           return "true";
+       else
+           return "false";
     }
 };
 
