3b1a5e01bea73e09ac1c5258ea29e060472f91b2
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2024-07-14 23:07:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        int i=0,j,n=arr.size();
+        while(arr[i]==0) i++;    //move i to first occurence of one
+        j=i;  //start j from i
+        while(j<n){
+            if(arr[j]==0) {swap(arr[i],arr[j]);i++;}  //swap 0 with first occurence of 1 and move i forward
+            j++; //move j forward
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        ob.segregate0and1(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
