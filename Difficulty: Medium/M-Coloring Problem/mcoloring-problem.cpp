a41cf86c1ee61fe84e56931f85d1bfc10e5dc7c4
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2024-08-17 19:47:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/M-Coloring Problem/mcoloring-problem.cpp
+++ Difficulty: Medium/M-Coloring Problem/mcoloring-problem.cpp
@@ -6,42 +6,39 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-   
-    bool isValid(bool graph[101][101], int node, int colour[], int n, int rang){
-       
-       for(int i=0; i<n; i++) {
-           if(i != node && graph[i][node] == 1 && colour[i] == rang)
-               return false;
-       }
-       return true;    
-   }
-
-   bool Coloring(bool graph[101][101], int m, int n, int colour[], int node){
-       if(node == n){
-           return true;
-       }
-       
-       for(int i=1; i<=m; i++){
-           if(isValid(graph,node,colour,n,i)){
-               colour[node] = i;
-               if(Coloring(graph,m,n,colour,node+1))
-                   return true;
-               colour[node] = 0;    
-           }
-       }
-       return false;
-   }
-   
-   bool graphColoring(bool graph[101][101], int m, int n) {
-       
-       int colour[n] = {0};
-       
-       if(Coloring(graph,m,n,colour,0))
-           return true;
-       return false;    
-   }
+    // Function to determine if graph can be coloured with at most M colours such
+    // that no two adjacent vertices of graph are coloured with same colour.
+    
+    
+    bool isSafe(bool graph[101][101], int n,  vector<int>&color, int node, int col){
+        for(int i = 0;i<n;i++){
+            if(i!=node && graph[node][i]==1 && color[i]==col) return false;
+        }
+        
+        return true;
+    }
+    
+    
+    bool solve(bool graph[101][101], int m, int n,  vector<int>&color, int node){
+        if(node==n) return true;
+        
+        for(int i = 1;i<=m;i++){
+            if(isSafe(graph, n, color, node, i)){
+                color[node] = i;
+                if(solve(graph,m,n,color,node+1)) return true;
+                color[node] = 0;
+            }
+        }
+        
+        return false;
+    }
+    bool graphColoring(bool graph[101][101], int m, int n) {
+        // your code here
+        vector<int> color(n,0);
+        if(solve(graph,m,n,color,0)) return true;
+        return false;
+    }
 };
-
 
 //{ Driver Code Starts.
 
