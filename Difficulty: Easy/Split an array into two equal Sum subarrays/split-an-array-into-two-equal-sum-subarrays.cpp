4beb54aacf177338ec12bce1165850cb9135d4a9
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2024-08-18 19:06:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool canSplit(vector<int>& arr) {
+        // code here
+        int sum = 0;
+        for(auto i: arr){
+            sum+=i;
+        }
+        
+        int sum2=0;
+        
+        for(auto i: arr){
+            sum2+=i;
+            sum-=i;
+            if(sum==sum2){
+                return true;
+            }
+        }
+        
+        
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t-- > 0) {
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> arr;
+        string temp;
+        while (ss >> temp) {
+            arr.push_back(stoi(temp));
+        }
+
+        Solution obj;
+        bool res = obj.canSplit(arr);
+        cout << (res ? "true" : "false") << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
