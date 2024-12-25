aaddffb915b9e248522ea8d7e8c4530c3f8cd3e6
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2024-12-25 10:58:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,6 +1,4 @@
 //{ Driver Code Starts
-// Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,40 +7,47 @@ using namespace std;
 // User function template for C++
 
 class Solution {
-    int di[4] = {1,0,0,-1};
-    int dj[4] = {0,-1,1,0};
     
-    void helper(vector<vector<int>> &mat, int i, int j, string&temp, vector<string>&ans){
-        int n = mat.size();
-        if(mat[i][j]!=1) return;
+    
+    void helper(vector<vector<int>> &mat, vector<string> &ans, 
+    string &path, int r, int c){
         
-        if(i==n-1 && j==n-1) {
-            ans.push_back(temp);
+        if(mat[r][c]!=1) return;
+        
+        int n = mat.size();
+        
+        if(r==n-1 && c==n-1){
+            ans.push_back(path);
             return;
         }
-        string dir="DLRU";
-        for(int k = 0;k<4;k++){
-            int nexti = i+di[k];
-            int nextj = j+dj[k];
-            if(nexti>=0 && nextj>=0 && nexti<n && nextj<n && mat[nexti][nextj]==1){
-                mat[i][j]=2;
-                temp+=dir[k];
-                helper(mat, nexti, nextj, temp, ans);
-                mat[i][j]=1;
-                temp.pop_back();
+        
+        string dir = "DLRU";
+        int rd []= {1,0,0,-1};
+        int cd []= {0,-1,1,0};
+        
+        for(int i = 0;i<4;i++){
+            int nextr = r + rd[i];
+            int nextc = c + cd[i];
+            
+            if(nextr>=0 and nextc>=0 && nextr<n && nextc<n && mat[nextr][nextc]==1){
+                mat[r][c] = 2;
+                path+=dir[i];
+                helper(mat, ans, path, nextr, nextc);
+                path.pop_back();
+                mat[r][c] = 1;
             }
+            
         }
     }
     
+    
   public:
     vector<string> findPath(vector<vector<int>> &mat) {
-        // Your code goes here
-        string temp = "";
+        // code here
         vector<string> ans;
-        int i = 0, j=0;
-        helper(mat, i, j, temp, ans);
+        string path = "";
+        helper(mat, ans, path, 0, 0);
         return ans;
-    
     }
 };
 
@@ -52,25 +57,58 @@ class Solution {
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> m(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> m[i][j];
+        string input;
+        getline(cin, input);
+        vector<vector<int>> mat;
+        string innerArray;
+        bool isInsideArray = false;
+
+        for (char c : input) {
+            if (c == '[') {
+                if (isInsideArray) {
+                    innerArray.clear();
+                }
+                isInsideArray = true;
+            } else if (c == ']') {
+                if (isInsideArray && !innerArray.empty()) {
+                    vector<int> row;
+                    stringstream ss(innerArray);
+                    int num;
+
+                    while (ss >> num) {
+                        row.push_back(num);
+                        if (ss.peek() == ',')
+                            ss.ignore();
+                        while (isspace(ss.peek()))
+                            ss.ignore();
+                    }
+
+                    mat.push_back(row);
+                    innerArray.clear();
+                }
+                isInsideArray = false;
+            } else if (isInsideArray) {
+                if (!isspace(c)) {
+                    innerArray += c;
+                }
             }
         }
+
         Solution obj;
-        vector<string> result = obj.findPath(m);
+        vector<string> result = obj.findPath(mat);
         sort(result.begin(), result.end());
-        if (result.size() == 0)
-            cout << -1;
+
+        if (result.empty())
+            cout << "[]";
         else
             for (int i = 0; i < result.size(); i++)
                 cout << result[i] << " ";
         cout << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
