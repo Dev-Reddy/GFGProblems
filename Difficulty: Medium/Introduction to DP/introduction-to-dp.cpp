65c15a371ba1066f6c6237966e9c7e5b52dea0a4
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2025-01-24 00:05:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Introduction to DP/introduction-to-dp.cpp
+++ Difficulty: Medium/Introduction to DP/introduction-to-dp.cpp
@@ -10,40 +10,45 @@ using namespace std;
 
 class Solution {
     
-     long long memo[10000]; 
-    long long mod = 1e9+7;
-  public:
-  
-    Solution() {
-        memset(memo, -1, sizeof(memo)); 
+    long long int mod = 1000000007; 
+    
+    long long int helper(vector<long long int> &memo, int n){
+        if(n<=1) return n;
+        
+        if(memo[n]!= -1) return memo[n];
+        
+        memo[n] = (helper(memo, n-1) + helper(memo, n-2))%mod;
+        
+        return memo[n];
+        
     }
+    
+    
+  public:
     long long int topDown(int n) {
         // code here
-        if(n<=1){
-            return n;
-        }
+        vector<long long int>memo(n+1, -1);
         
-        if(memo[n]!=-1) return memo[n];
+        return helper(memo, n);
         
-        
-        memo[n] = (topDown(n-1) + topDown(n-2))%mod;
-        return memo[n];
     }
     long long int bottomUp(int n) {
         // code here
-        vector<long long int>dp(n+1);
+        if(n<=1) return n;
         
-        dp[0] = 0;
-        dp[1] = 1;
+        long long int a = 0;
+        long long int b = 1;
         
         for(int i = 2;i<=n;i++){
-            dp[i] = (dp[i-1] + dp[i-2])%mod;
+            long long int c = (a+b)%mod;
+            a = b;
+            b = c;
         }
         
-        return dp[n];
-        
+        return b;
     }
 };
+
 
 //{ Driver Code Starts.
 
