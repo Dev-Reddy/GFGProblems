e322bf039932727e41da8b01ec402ffc12624869
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2025-07-22 23:52:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.cpp
@@ -1,52 +1,23 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+  public:
+    int missingNumber(vector<int> &arr) {
+        int n = arr.size();
+        int i = 0;
 
+        while (i < n) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= n && arr[i] != arr[correctIndex]) {
+                swap(arr[i], arr[correctIndex]);
+            } else {
+                i++;
+            }
+        }
 
-// } Driver Code Ends
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1)
+                return i + 1;
+        }
 
-class Solution
-{
-    public:
-    //Function to find the smallest positive number missing from the array.
-    int missingNumber(int arr[], int n) 
-    { 
-        // Your code here
-        sort(arr,arr+n);
-          int count=1;
-       for (int i=0;i<n;i++){
-           if (arr[i]==count)
-           count++;
-       }
-       return count;
-        
-    } 
-};
-
-
-//{ Driver Code Starts.
-
-int missingNumber(int arr[], int n);
-
-int main() { 
-    
-    //taking testcases
-    int t;
-    cin>>t;
-    while(t--){
-        
-        //input number n
-        int n;
-        cin>>n;
-        int arr[n];
-        
-        //adding elements to the array
-        for(int i=0; i<n; i++)cin>>arr[i];
-        
-        Solution ob;
-        //calling missingNumber()
-        cout<<ob.missingNumber(arr, n)<<endl;
+        return n + 1;
     }
-    return 0; 
-} 
-// } Driver Code Ends
+};
