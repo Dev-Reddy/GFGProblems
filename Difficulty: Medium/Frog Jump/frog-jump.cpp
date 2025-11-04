6a825210e410b6756ad39e6d057b207fcb5ddee3
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2025-11-04 23:47:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,55 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-    
-    int helper(const vector<int>& height, int i, vector<int>& memo){
-        if(i==0) return 0;
-        
-        if(memo[i]!=-1) return memo[i];
-        
-        memo[i] = min(abs(height[i-1]-height[i])+helper(height, i-1, memo), 
-        i>1? abs(height[i-2]-height[i])+helper(height, i-2, memo): INT_MAX);
-        
-        return memo[i];
-    }
-    
-    
   public:
     int minCost(vector<int>& height) {
         // Code here
-        int n = height.size();
-        if(n==1) return 0;
+         vector<int> dp(height.size(), 0);
         
-        vector<int>memo(n,-1);
+        for(int i = height.size()-2;i>=0;i--){
+            int a = abs(height[i]-height[i+1]) + dp[i+1];
+            int b = INT_MAX;
+            if (i + 2 < height.size())
+                b = abs(height[i] - height[i + 2]) + dp[i + 2];
+            
+            dp[i] = min(a,b);
+        }
         
-        return helper(height, n-1, memo);
+        return dp[0];
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    string str;
-    getline(cin, str);
-    int t = stoi(str);
-    while (t--) {
-        getline(cin, str);
-        stringstream ss(str);
-        vector<int> arr;
-        int num;
-        while (ss >> num) {
-            arr.push_back(num);
-        }
-        Solution ob;
-        cout << ob.minCost(arr) << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
