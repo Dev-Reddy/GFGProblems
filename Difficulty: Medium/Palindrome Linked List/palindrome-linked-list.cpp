ceb959ff52a549100868b0272dad1bbb928021c4
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2025-12-31 23:31:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.cpp
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.cpp
@@ -1,132 +1,53 @@
-//{ Driver Code Starts
-#include <algorithm>
-#include <bits/stdc++.h>
-#include <cmath>
-#include <cstdio>
-#include <ios>
-#include <iostream>
-#include <random>
-#include <sstream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-struct Node {
+/*
+class Node {
+  public:
     int data;
-    struct Node *next;
+    Node *next;
 
     Node(int x) {
-        data = x;
-        next = NULL;
+       data = x;
+       next = NULL;
     }
-};
-
-
-// } Driver Code Ends
-/*
-struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
 };
 */
 
 class Solution {
-  public:
-    // Function to check whether the list is palindrome.
-    bool isPalindrome(Node *head) {
-        // Your code here
+    Node* ReverseList (Node* temp) {
         Node* prev = NULL;
-        Node* curr = head;
+        Node* curr = temp;
+        Node* nex = temp;
+        while (curr) {
+            nex = nex->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nex;
+        }
+        return prev;
+    }
+  public:
+    bool isPalindrome(Node *head) {
+        //  code here
+        Node* slow = head;
         Node* fast = head;
-        Node* temp;
-        
-        while(fast->next && fast->next->next)
-        {
-            fast = fast->next->next;
-            temp = curr;
-            curr = curr->next;
-            temp ->next = prev;
-            prev = temp;
+        Node* temp = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast->next) {
+                fast = fast->next;
+            }
         }
-        if(!fast->next)
-        {
-            curr = curr->next;
+        if (fast == NULL) {
+            slow = slow->next;
         }
-        else
-        {
-            temp = curr;
-            curr = curr->next;
-            temp->next = prev;
-            prev = temp;
-        }
-        while(curr)
-        {
-            if(curr->data != prev->data)
+        slow = ReverseList(slow);
+        while (slow) {
+            if (temp->data != slow->data) {
                 return false;
-            curr = curr->next;
-            prev = prev->next;
+            }
+            slow = slow->next;
+            temp = temp->next;
         }
         return true;
     }
 };
-
-
-//{ Driver Code Starts.
-
-/* Function to print nodes in a given linked list */
-void printList(struct Node *head) {
-    struct Node *temp = head;
-    while (temp != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        if (arr.empty()) {
-            cout << "empty" << endl;
-            continue;
-        }
-
-        struct Node *head = new Node(arr[0]);
-        struct Node *tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            tail->next = new Node(arr[i]);
-            tail = tail->next;
-        }
-        Solution ob;
-        if (ob.isPalindrome(head))
-            cout << "true" << endl;
-        else
-            cout << "false" << endl;
-
-        // Clean up the remaining nodes to avoid memory leaks
-        while (head != NULL) {
-            struct Node *temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
-    return 0;
-}
-
-// } Driver Code Ends
