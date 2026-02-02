47512272f5c1be6412f6612ea4b4e457793f9041
DEV <91939359+Dev-Reddy@users.noreply.github.com>
2026-02-03 00:10:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.cpp
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.cpp
@@ -1,49 +1,14 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int maximumProfit(vector<int> &prices) {
+    int maxProfit(vector<int> &arr) {
         // code here
-        int maxCount = 0;
-        int buyPrice = prices[0];
-        for(int i = 1;i<prices.size();i++) {
-            if(prices[i] > buyPrice) {
-                maxCount =  max(prices[i] - buyPrice,maxCount);
-            } else {
-                buyPrice =  prices[i];
-            } 
+         int min=INT_MAX,p=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<min){ 
+                min=arr[i];
+            }
+            else if((arr[i] - min) > p) p =arr[i]-min; 
         }
-        
-        return maxCount;
+        return p;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> prices;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            prices.push_back(number);
-        }
-
-        Solution ob;
-        int ans = ob.maximumProfit(prices);
-        cout << ans << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
